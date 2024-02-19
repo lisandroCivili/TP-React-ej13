07867c9c23398bd9672c5e7fba6b8d87544976ba
Lisandro Civili <lisandrocivili13@gmail.com>
2024-02-18 22:52:42 -0300

    Limpio proyecto y agrego etiquetas meta

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Container from 'react-bootstrap/Container' 
+import Titulo from './components/Titulo'
+
+function App() {
+
+  return (
+    <>
+      <Titulo></Titulo>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <div className='text-center py-3 border border-black'>
+            <h1>ClimaNet</h1>
+        </div>
+    );
+};
+
+export default Titulo;
